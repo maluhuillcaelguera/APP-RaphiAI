eedd1af7d3c174537cc9b3ceb6aa9b88fb1d97cc
sindimalu <frontend-8@medialabla.com>
2025-07-02 23:09:52 -0500

    ajustes carpeta

--- html/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="text-center p-10">
-      <h1 className="text-2xl font-bold mb-4">Hola mundo</h1>
-      <p>Bienvenido a tu app para analizar hojas de papa 🌿</p>
-    </div>
-  )
-}
---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>¡Hola, React está funcionando!</h1>
+      <p>Este es un proyecto base listo para ejecutar.</p>
+    </div>
+  );
+}
+
+export default App;
